20ef9ea9c359e9b311d87b77851a94cf9d7a88a3
Lape15 <larape1993@gmail.com>
2020-08-21 13:17:01 +0100

    updates

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import "./collection.styles.scss";
 import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>collection PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
