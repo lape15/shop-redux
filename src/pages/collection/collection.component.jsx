4cf7cfbc324936872e9adf30c4b3f883cf19877d
Lape15 <larape1993@gmail.com>
2020-08-21 12:57:59 +0100

    updates

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "react-redux";
+import "./collection.styles.scss";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  return (
+    <div className="collection-page">
+      <h2>collection PAGE</h2>
+    </div>
+  );
+};
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
-import CollectionPage from "../category/collection.component";
+import CollectionPage from "../collection/collection.component";
 const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.url}`} component={CollectionOverview} />
+      <Route path={`${match.url}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
